2f55e78c35821cdcdaf78c563c910987990a5448
Jason Van Pelt <jvanpelt@newrelic.com>
2016-12-16 20:37:43 -0800

    promote pawns and recognize check

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,7 +11,7 @@ require('./App.scss')
 const lightSquareColor = '#2492FF'
 const darkSquareColor = '#005EBB'
 const flip = false
-const squareSize = 50
+const squareSize = 70
 
 export class App extends React.Component {
   constructor (props) {
@@ -31,11 +31,30 @@ export class App extends React.Component {
   }
 
   onMovePiece = (piece, fromSquare, toSquare) => {
-    this.chess.move({from: fromSquare, to: toSquare})
+    const game = this
+    let move = fromSquare + toSquare
+    const type = piece.toLowerCase()
+    if (type === 'p' && ([1,8]).includes(parseInt(toSquare[1]))) {
+      let newType = ''
+      while (!(['q', 'r', 'b', 'n']).includes(newType.toLowerCase())) {
+        newType = prompt('New piece (q, b, r, n)')
+      }
+      const newPiece = (type !== piece) ? newType.toLowerCase() : newType.toUpperCase()
+      move += newPiece
+    }
+
+    this.chess.move(move, {sloppy: true})
     if (this.chess.fen() === this.state.fen) {
       this.setState({lastMove: 'Invalid move'})
     } else {
       this.setState({fen: this.chess.fen(), lastMove: `${piece}${fromSquare}${toSquare}`})
+      setTimeout(function () {
+        if (game.chess.in_checkmate()) {
+          alert('Check mate!')
+        } else if (game.chess.in_check()) {
+          alert('Check!')
+        }
+      }, 0)
     }
   }
 
