af82628c41d7453bfae28f813e59359d6e98f5b3
Jason Van Pelt <jvanpelt@newrelic.com>
2016-12-17 08:41:12 -0800

    add verbose option for trainer

--- src/server/trainer.js
+++ src/server/trainer.js
@@ -15,13 +15,14 @@ const optionDefinitions = [
   {name: 'help', alias: 'h', type: Boolean},
   {name: 'fen', alias: 'f', type: String},
   {name: 'moves', alias: 'm', type: String},
-  {name: 'movetime', alias: 't', type: Number}
+  {name: 'movetime', alias: 't', type: Number},
+  {name: 'verbose', alias: 'v', type: Boolean}
 ]
 
 const options = commandLineArgs(optionDefinitions)
 options.movetime = options.movetime || 3
 
-console.log('options: ' + JSON.stringify(options))
+logIfVerbose('options: ' + JSON.stringify(options))
 
 if (options.help) {
   printHelp()
@@ -39,37 +40,37 @@ if (options.moves) {
 engine.onmessage = processMessage
 
 function send (str) {
-  console.log("Sending: " + str)
+  logIfVerbose("Sending: " + str)
   engine.postMessage(str)
 }
 
-console.log('Position: ' + position)
+logIfVerbose('Position: ' + position)
 send("uci")
 
 
 function processMessage (line) {
   var match
-  console.log("Line: " + line)
+  logIfVerbose("Line: " + line)
 
   if (typeof line !== "string") {
-    console.log("Got line:")
-    console.log(typeof line)
-    console.log(line)
+    logIfVerbose("Got line:")
+    logIfVerbose(typeof line)
+    logIfVerbose(line)
     return
   }
 
   if (!got_uci && line === "uciok") {
     got_uci = true
-    console.log('setup new game')
+    logIfVerbose('setup new game')
     if (position) {
       send("position " + position)
       send("eval")
       send("d")
     }
-    console.log('go ponder')
+    logIfVerbose('go ponder')
     send("go ponder")
   } else if (!started_thinking && line.indexOf("info depth") > -1) {
-    console.log("Thinking...")
+    logIfVerbose("Thinking...")
     started_thinking = true
     setTimeout(function () {
       send("stop")
@@ -82,17 +83,22 @@ function processMessage (line) {
       duration: duration,
       options: options
     }
+    // report output
     console.log(JSON.stringify(output))
     process.exit()
   }
 }
 
 function printHelp () {
-  console.log("Usage: node trainer.js [--fen FEN-string] [--moves \"move move move\"] [--movetime seconds]")
-  console.log("")
-  console.log("Examples:")
-  console.log("   node trainer.js")
-  console.log("   node trainer.js -f \"rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2\"")
-  console.log("   node trainer.js -m \"g1f3 e7e\"")
-  console.log("   node trainer.js -t 10")
+  logIfVerbose("Usage: node trainer.js [--fen FEN-string] [--moves \"move move move\"] [--movetime seconds] [--verbose]")
+  logIfVerbose("")
+  logIfVerbose("Examples:")
+  logIfVerbose("   node trainer.js")
+  logIfVerbose("   node trainer.js -f \"rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2\"")
+  logIfVerbose("   node trainer.js -m \"g1f3 e7e\"")
+  logIfVerbose("   node trainer.js -t 10")
+}
+
+function logIfVerbose (msg) {
+  if (options.verbose) console.log(msg)
 }
